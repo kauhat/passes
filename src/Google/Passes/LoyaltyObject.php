fbe1a4ef7dcbc61a7e426b1035637337b45a66ee
Jack Fletcher <jack.fletcher@sbgl.co.uk>
2024-11-04 10:20:51 +0000

    Merge changes from upstream

--- src/Google/Passes/LoyaltyObject.php
+++ src/Google/Passes/LoyaltyObject.php
@@ -3,47 +3,48 @@
 namespace Chiiya\Passes\Google\Passes;
 
 use Chiiya\Passes\Google\Components\Loyalty\LoyaltyPoints;
+use Symfony\Component\Validator\Constraints\Length;
 
 class LoyaltyObject extends BaseObject
 {
     /** @var string */
     final public const IDENTIFIER = 'loyaltyObject';
 
-    /**
-     * Optional.
-     * A copy of the inherited fields of the parent class. These fields are retrieved during a GET.
-     */
-    public ?LoyaltyClass $classReference;
-
-    /**
-     * Optional.
-     * The loyalty account holder name, such as "John Smith." Recommended maximum length is 20 characters
-     * to ensure full string is displayed on smaller screens.
-     */
-    public ?string $accountName;
-
-    /**
-     * Optional.
-     * The loyalty account identifier. Recommended maximum length is 20 characters.
-     */
-    public ?string $accountId;
-
-    /**
-     * Optional.
-     * The loyalty reward points label, balance, and type.
-     */
-    public ?LoyaltyPoints $loyaltyPoints;
-
-    /**
-     * Optional.
-     * A list of offer objects linked to this loyalty card. The offer objects must already exist. Offer object IDs
-     * should follow the format issuer ID.identifier where the former is issued by Google and latter is chosen by you.
-     */
-    public array $linkedOfferIds = [];
-
-    /**
-     * Optional.
-     * The secondary loyalty reward points label, balance, and type. Shown in addition to the primary loyalty points.
-     */
-    public ?LoyaltyPoints $secondaryLoyaltyPoints;
+    public function __construct(
+        /**
+         * Optional.
+         * A copy of the inherited fields of the parent class. These fields are retrieved during a GET.
+         */
+        public ?LoyaltyClass $classReference = null,
+        /**
+         * Optional.
+         * The loyalty account holder name, such as "John Smith." Recommended maximum length is 20 characters
+         * to ensure full string is displayed on smaller screens.
+         */
+        public ?string $accountName = null,
+        /**
+         * Optional.
+         * The loyalty account identifier. Recommended maximum length is 20 characters.
+         */
+        public ?string $accountId = null,
+        /**
+         * Optional.
+         * The loyalty reward points label, balance, and type.
+         */
+        public ?LoyaltyPoints $loyaltyPoints = null,
+        /**
+         * Optional.
+         * A list of offer objects linked to this loyalty card. The offer objects must already exist. Offer object IDs
+         * should follow the format issuer ID.identifier where the former is issued by Google and latter is chosen by you.
+         */
+        public array $linkedOfferIds = [],
+        /**
+         * Optional.
+         * The secondary loyalty reward points label, balance, and type. Shown in addition to the primary loyalty points.
+         */
+        public ?LoyaltyPoints $secondaryLoyaltyPoints = null,
+        ...$args,
+    ) {
+        parent::__construct(...$args);
+    }
 }
